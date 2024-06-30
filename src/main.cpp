077ff614a3678af92e7071c938d9c279a5ca2bd9
JoeMudr <gh@joesuse.de>
2024-06-30 20:42:05 +0200

    Add configurable Angles; Fix negative values; Change Stepper_Drive to aangle; Reduce stepper speed.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <Stepper.h>
 #include <EEPROM.h>
 
-#define Firmware 240620
+#define Firmware 240630
 
-#define EEPROM_ConfVersion 5
+#define EEPROM_ConfVersion 6
 
 struct conf {
   uint8_t ConfVersion;
@@ -17,20 +17,24 @@ struct conf {
   float ValMin;
   float ValMax;
   uint16_t Boudrate;
+  uint16_t AngleMin;
+  uint16_t AngleMax;
 };
 
 conf Settings;
 
 can_frame canMsg;
 MCP2515 CAN(10); // CS PIN 10
+#define MCP_Crystal MCP_8MHZ
 
-#define  STEPS  720    // steps per revolution (630 = limited to 315°)
-#define  COIL1  6
-#define  COIL2  7
-#define  COIL3  8
-#define  COIL4  9
-#define  max_Steps 630
-
+#define STEPS  720    // steps per revolution (630 = limited to 315°)
+#define COIL1  6
+#define COIL2  7
+#define COIL3  8
+#define COIL4  9
+#define max_Steps 630
+#define max_Angle 315 // calculating from  STEPS & max_Steps somehow leads to an integer overflow. So ist static here.
+#define Stepper_speed 40
 
 Stepper stepper(STEPS, COIL1, COIL2, COIL3, COIL4);
 
@@ -44,7 +48,7 @@ bool ValDebug = false;
 String incomingByte; 
 
 void Config();
-void Stepper_Drive(float _val);
+void Stepper_Drive(double _val);
 void Serial_clear();
 void Can_Debug(can_frame canMsg);
 void Can_Decode(can_frame canMsg);
@@ -62,14 +66,15 @@ void setup() {
     Settings.ValMin = 0.0;
     Settings.ValMax = 0.0;
     Settings.Boudrate = 500;
+    Settings.AngleMin = 0;
+    Settings.AngleMax = 315;
   }
+  EEPROM.put(0, Settings);
   CAN_reset();
-  stepper.setSpeed(50);   // set the motor speed to 30 RPM (360 PPS aprox.).
+  stepper.setSpeed(Stepper_speed);   // set the motor speed to 30 RPM (360 PPS aprox.).
   stepper.step(max_Steps);
   delay(20);
   stepper.step(-max_Steps);
-  //Stepper_Drive(100);
-  //Stepper_Drive(0);
   Serial.begin(115200);
   if(Serial){
     Config();
@@ -86,7 +91,7 @@ void loop() {
 
 void CAN_reset(){
   CAN.reset();
-  CAN.setBitrate(Settings.Boudrate, MCP_16MHZ); // Change to correct Clock!
+  CAN.setBitrate(Settings.Boudrate, MCP_Crystal); // Change to correct Clock!
   CAN.setNormalMode();
 }
 
@@ -121,11 +126,13 @@ void Config(){
     case 5: Settings.Offset = menu_option_val_Int; Config();break;
     case 6: Settings.ValMin = menu_option_val_Float; Config();break;
     case 7: Settings.ValMax = menu_option_val_Float; Config();break;
-    case 8: Settings.Boudrate = menu_option_val_Int; CAN_reset(); Config();break;
-    case 9: Stepper_Drive(constrain(menu_option_val_Int,0,100)); Config();break;
-    case 10: CanDebug = !CanDebug; Config(); break;
-    case 11: ValDebug = !ValDebug; Config(); break;
-    case 12: EEPROM.put(0, Settings); Config(); Serial.println(":::::Saved:::::");break;
+    case 8: Settings.AngleMin = constrain(menu_option_val_Int,0,max_Angle); Config();break;
+    case 9: Settings.AngleMax = constrain(menu_option_val_Int,0,max_Angle); Config();break;
+    case 10: Settings.Boudrate = menu_option_val_Int; CAN_reset(); Config();break;
+    case 11: Stepper_Drive(constrain(menu_option_val_Int,0,max_Angle)); Config();break;
+    case 12: CanDebug = !CanDebug; Config(); break;
+    case 13: ValDebug = !ValDebug; Config(); break;
+    case 14: EEPROM.put(0, Settings); Config(); Serial.println(":::::Saved:::::");break;
     default:
       Serial_clear();
       Serial.print("Firmware: "); Serial.println(Firmware);
@@ -135,13 +142,15 @@ void Config(){
       Serial.print("[3]  Bits:           ");Serial.println(Settings.Bits);
       Serial.print("[4]  Gain:           ");Serial.println(Settings.Gain);
       Serial.print("[5]  Offset:         ");Serial.println(Settings.Offset);
-      Serial.print("[6]  Minimum:        ");Serial.println(Settings.ValMin);
-      Serial.print("[7]  Maximum:        ");Serial.println(Settings.ValMax);
-      Serial.print("[8]  Boud Rate (KB): ");Serial.println(Settings.Boudrate);
-      Serial.print("[9]  Gauge Test (%)");Serial.println();
-      Serial.print("[10] CAN Test:       "+String(CanDebug?"ON":"OFF"));Serial.println();
-      Serial.print("[11] Value Test:     "+String(ValDebug?"ON":"OFF"));Serial.println();
-      Serial.print("[12] Safe Settings!");Serial.println();
+      Serial.print("[6]  Min. Value:     ");Serial.println(Settings.ValMin);
+      Serial.print("[7]  Max. Value:     ");Serial.println(Settings.ValMax);
+      Serial.print("[8]  Min. Angle:     ");Serial.println(Settings.AngleMin);
+      Serial.print("[9]  Max. Angle:     ");Serial.println(Settings.AngleMax);
+      Serial.print("[10] Boud Rate (KB): ");Serial.println(Settings.Boudrate);
+      Serial.print("[11] Gauge Test (°)  ");Serial.println();
+      Serial.print("[12] CAN Test:       "+String(CanDebug?"ON":"OFF"));Serial.println();
+      Serial.print("[13] Value Test:     "+String(ValDebug?"ON":"OFF"));Serial.println();
+      Serial.print("[14] Safe Settings!");Serial.println();
       Serial.println();
       Serial.println();
       Serial.println("Result = (CAN Value x Gain) + Offset");
@@ -161,13 +170,13 @@ void Serial_clear(){
   //for (byte i = 0; i < 40; i++){Serial.println();}
 }
 
-// pass a value between 0 & 100 (%).
-void Stepper_Drive(float _val){
-  int rel_pos = 0;    
+// pass a value between 0 & max_Steps*360/STEPS (°).
+void Stepper_Drive(double _val){
+  int16_t rel_pos = 0;    
 
-  _val = _val * max_Steps / 100;
+  _val = _val * max_Steps / 360;
   _val = constrain(_val,0,max_Steps);
-  rel_pos = _val-abs_pos;
+  rel_pos = round(_val)-abs_pos;
   abs_pos = _val;
   stepper.step(rel_pos);
 }
@@ -189,28 +198,42 @@ void Can_Debug(can_frame canMsg){
 void Can_Decode(can_frame canMsg){
   uint64_t tmp_val = 0;
   int32_t val = 0;
+  double result = 0.0;
+
   for (byte i = 0; i < canMsg.can_dlc; i++){
     tmp_val |= uint64_t(canMsg.data[i]) << (8*i);
   }
+
   
   if(Settings.Bits > 0){
     // Little Endian
-    uint64_t mask = (1ULL << Settings.Bits) - 1;
-    val = tmp_val >> (Settings.Position) & mask;
+    uint32_t mask = (1UL << Settings.Bits) - 1;  
+    val = (tmp_val >> Settings.Position) & mask;
   } else {
     // Big Endian 
     for (int i = (Settings.Position + Settings.Bits); i <= Settings.Position; ++i) {
       val = (val << 1) | ((tmp_val >> i) & 1);
     }
   }
-  val = (val * Settings.Gain) + Settings.Offset;
-  val = constrain(val, Settings.ValMin, Settings.ValMax);
 
+  if(Settings.ValMin < 0){   
+    if (val & (1 << (Settings.Bits - 1))) { // Signed
+      val -= (1UL << Settings.Bits);
+    }
+  }
+  
+  result = (double(val) * Settings.Gain) + Settings.Offset;
   if(ValDebug){
-    Serial.println(val);
+    Serial.println(result);
   }
 
-  val = map(val, Settings.ValMin, Settings.ValMax, 0, 100);
+  result = constrain(result, Settings.ValMin, Settings.ValMax);
+  // map() can't handle float. So multiply by 100 & typecast. Then devide by 100.
+  result = double(map(result*100, Settings.ValMin*100, Settings.ValMax*100, Settings.AngleMin*100, Settings.AngleMax*100))/100; 
 
-  Stepper_Drive(val);
+  if(ValDebug){
+    Serial.print(result);Serial.println("°");
+  }
+
+  Stepper_Drive(result);
 }
